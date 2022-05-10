72e0addda5228d7be7889de4cb39b84056c45c64
tatulyka <tatulyka@gmail.com>
2022-05-10 18:18:11 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
   "use strict";
 
   function ValidateEmail() {
-    let mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/i;
+    let mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
     let email = document.getElementById('businessmail').value;
     let valid = mailFormat.test(email);
-    if (valid) output = "The email is correct";
-    else output = "Please enter your correct email";
-    document.getElementById('errormessage2').innerHTML += output;
-        return valid;
-    }
-  //let errors = [];
-   // function ValidatePass(password) {
-     // let pass = document.getElementById('password').value;
-     // let passvalidity = pass;
+    const output = valid ? "The email is correct" : "Please enter your correct email";
+    document.getElementById('errormessage2').innerHTML = output;
+    return valid;
+  }
+  
+  function ValidatePass() {
+    let passFormat = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/;
+    let pass = document.getElementById('password').value;
+    let valid = passFormat.test(pass);
+    const output2 = valid ? "The password is strong" : "password is not strong";
+    document.getElementById('errormessage3').innerHTML = output2;
+    return valid;
+
+   }
 
 
       
