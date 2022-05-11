701784711ca2cfe675920905ac3cc0881cb5de42
tatulyka <tatulyka@gmail.com>
2022-05-11 17:38:17 +0200

    rangeOverflow

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
   "use strict";
+  function validate() {
+     
+      let name = document.getElementById('businessname').value;
+      let nameValidity = name.validity;
+      const output1 = '';
+      if (nameValidity.rangeOverflow) 
+		{ let max = name.max;
+			output1 = 'title length cannot be longer';
+      document.getElementById('errormessage1').innerHTML = output1 + max; }
+      
+      if (nameValidity.rangeUnderflow) 
+      { let min = name.min;
+        output1 = 'title length cannot be shorter';
+        document.getElementById('errormessage1').innerHTML = output1 + min; };
+			
 
-  function ValidateEmail() {
+     
+
+
     let mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
     let email = document.getElementById('businessmail').value;
     let valid = mailFormat.test(email);
     const output = valid ? "The email is correct" : "Please enter your correct email";
     document.getElementById('errormessage2').innerHTML = output;
-    return valid;
-  }
-  
-  function ValidatePass() {
+    
     let passFormat = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/;
     let pass = document.getElementById('password').value;
-    let valid = passFormat.test(pass);
-    const output2 = valid ? "The password is strong" : "password is not strong";
+    let valid1 = passFormat.test(pass);
+    const output2 = valid1 ? "The password is strong" : "The password is not strong";
     document.getElementById('errormessage3').innerHTML = output2;
-    return valid;
 
+
+
+
+
+    
    }
 
 
