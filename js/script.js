fd1e38817f3724bf03ebcc7395bf2115c5039c6d
tatulyka <tatulyka@gmail.com>
2022-05-12 11:50:24 +0200

    businessname

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
      
       let name = document.getElementById('businessname').value;
       let nameValidity = name.validity;
-      let min = name.min;
-      const output1 = nameValidity.rangeUnderflow ? "title length cannot be shorter" : "Is OK";
-      document.getElementById('errormessage1').innerHTML = output1 + min; 		
+     let min = name.length;
+     const output1 = nameValidity.tooShort ? "title length cannot be shorter than" : "Is OK";
+     document.getElementById('errormessage1').innerHTML = output1 + min; 		
     
     let mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
     let email = document.getElementById('businessmail').value;
